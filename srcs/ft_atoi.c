1e42903c5a87c5d6665b744c292d1733495376cf
Milvintsiss <milvintsiss@gmail.com>
2022-11-09 00:53:32 +0100

    Fixes overflow for ft_atoi

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,15 +6,21 @@
 /*   By: ple-stra <ple-stra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 04:19:11 by ple-stra          #+#    #+#             */
-/*   Updated: 2022/05/06 05:49:03 by ple-stra         ###   ########.fr       */
+/*   Updated: 2022/11/09 00:53:05 by ple-stra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/// @brief Converts an alphabetic string to an int.
+/// @param nptr 
+/// @return Returns the integer version of number contained by nptr.
+/// Returns -1 in case of overflow of a positive number.
+/// Returns 1 in case of overflow of a negative number.
 int	ft_atoi(const char *nptr)
 {
 	int	nb;
+	int	nb_prec;
 	int	neg;
 
 	nb = 0;
@@ -25,11 +31,12 @@ int	ft_atoi(const char *nptr)
 		nptr++;
 	while (*nptr && ft_isdigit(*nptr))
 	{
-		if (nb * 10 < nb)
-			return (-1 * (neg != -1));
-		nb = nb * 10 + *(nptr++) - '0';
+		nb_prec = nb;
+		nb = nb * 10 + (*(nptr++) - '0') * neg;
+		if ((nb_prec > nb && neg == 1) || (nb_prec < nb && neg == -1))
+			return (-1 * neg);
 	}
-	return (nb * neg);
+	return (nb);
 }
 
 static int	pos_in_base(const char c, char const *base)
